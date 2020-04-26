cf5eef999b40d64a08cc1ded0e1a79601e1f4a15
Marissa Gonçalves <marissagoncalves13@gmail.com>
2020-04-26 00:56:28 -0400

    Commit once more.

--- app/controllers/PromotionController.php
+++ app/controllers/PromotionController.php
@@ -6,15 +6,15 @@
 
 class PromotionController extends Controller
 {
-    public function index($product_id)
+    public function index()
     {
         $user_id = (string) $_SESSION['user_id'];
         $theProfile = $this->model('Profile')->findProfile($user_id);
         $_SESSION['profile_id'] = $theProfile->profile_id;
-        //$theProduct = $this->model('Product')->getProductForSeller($_SESSION['profile_id']);
-        //$_SESSION['product_id'] = $theProduct->product_id;
-        $promotions = $this->model('Promotion')->getPromotionForSeller($product_id, $_SESSION['profile_id']);
-        $this->view('promotion/index', ['promotions'=>$promotions]);
+        $theProduct = $this->model('Product')->getProductForSeller($_SESSION['profile_id']);
+        $_SESSION['product_id'] = $theProduct->product_id;
+        $promotions = $this->model('Promotion')->getPromotionForSeller($_SESSION['product_id'], $_SESSION['profile_id']);
+        $this->view('promotion/index'.$_SESSION['product_id'], ['promotions'=>$promotions]);
     }
 
     public function create()
@@ -27,7 +27,7 @@ class PromotionController extends Controller
             $newPromotion->seller_id= $_SESSION['profile_id'];
             $newPromotion->product_id= $_SESSION['product_id'];
             $newPromotion->create();
-            header('location:/promotion/index');
+            header('location:/promotion/index'.$newPromotion->product_id);
         }
 
         else
