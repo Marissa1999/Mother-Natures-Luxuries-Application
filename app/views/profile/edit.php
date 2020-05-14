9b34ccd90c61991baf3a3742ee2dc6479adb3ee4
amy-yip <amy.chauyip@gmail.com>
2020-05-14 18:58:53 -0400

    adjusted the radio buttons in the views/profile/edit layout

--- app/views/profile/edit.php
+++ app/views/profile/edit.php
@@ -24,6 +24,13 @@
               align-items: center;
               justify-content: center;
           }
+          .radio-group{
+              margin-bottom:40px;
+              height: 100%;
+              text-align: center;
+              align-items: center;
+              justify-content: center;
+          }
           .save-button {
               margin-bottom:50px;
               height: 100%;
@@ -31,21 +38,25 @@
               align-items: center;
               justify-content: center;
           }
+          label {
+              height: 100%;
+              display: inline;
+              text-align: center;
+          }
           p {
               height: 100%;
               display: flex;
               align-items: center;
               justify-content: center;
           }
-          label,
-          input {
-              height: 100%;
-              display: block;
-              text-align: center;
-          }
           body {
               background-color: lavender;
+              display: ;
               font-family: Helvetica, sans-serif;
+          }
+          input[type="radio"] + label {
+              display:inline-block;
+              vertical-align:middle;
           }
       </style>
     <title>Edit the Profile</title>
@@ -55,27 +66,54 @@
        <h1>Edit the Profile</h1>
        <form action='' method='post'>
         <div class='form-group'>
-             <label>First Name: <input type='text' name='first_name' value='<?=$data->first_name ?>' class='form-control' /></label>
+            <label style="font-weight: bold">First Name <input type='text' name='first_name' value='<?=$data->first_name ?>' class='form-control' /></label>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <label>Last Name: <input type='text' name='last_name' value='<?=$data->last_name ?>' class='form-control' /></label>
+            <label style="font-weight: bold">Last Name <input type='text' name='last_name' value='<?=$data->last_name ?>' class='form-control' /></label>
         </div>
         <div class='form-group'>
-            <label>Email: <input type='text' name='email' value='<?=$data->email ?>' class='form-control' /></label>
+            <label style="font-weight: bold">Email <input type='text' name='email' value='<?=$data->email ?>' class='form-control' /></label>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-            <label>Phone Number: <input type='text' name='phone_number' value='<?=$data->phone_number ?>' class='form-control' /></label>
+            <label style="font-weight: bold">Phone Number <input type='text' name='phone_number' value='<?=$data->phone_number ?>' class='form-control' /></label>
+        </div>
+        <div class="radio-group">
+            <label style="font-weight: bold">Theme</label><br/>
+            <input type="radio" name="theme_id"  value="1" <?php echo $data->theme_id == '1' ? 'checked' : ''?>><label>&nbsp;Beauty</label>
+            <input type="radio" name="theme_id"  value="2"
+                <?php echo $data->theme_id == '2' ? 'checked' : ''?>><label>&nbsp;Medical</label>
+            <input type="radio" name="theme_id"  value="3"
+                <?php echo $data->theme_id == '3' ? 'checked' : ''?>><label>&nbsp;Tea</label>
+        </div>
+        <div class="radio-group">
+            <label style="font-weight: bold">Gender</label><br/>
+            <input type="radio" name="gender"  value="Male"
+                <?php echo $data->gender == 'Male' ? 'checked' : ''?>><label>&nbsp;Male</label>
+            <input type="radio" name="gender"  value="Female"
+                <?php echo $data->gender == 'Female' ? 'checked' : ''?>><label>&nbsp;Female</label>
+        </div>
+        <div class="radio-group">
+            <label style="font-weight: bold">User Type</label><br/>
+            <input type="radio" name="user_type"  value="Buyer"
+                <?php echo $data->user_type == 'Buyer' ? 'checked' : ''?>><label>&nbsp;Buyer</label>
+            <input type="radio" name="user_type"  value="Seller"
+                <?php echo $data->user_type == 'Seller' ? 'checked' : ''?>><label>&nbsp;Seller</label>
         </div>
 
+        <div class="form-group">
+            <label>Location <input type='text' name='location' value='<?=$data->location ?>' class='form-control' size="50"/></label>
+        </div>
+<!--
          <div class='form-group'>
-            <label>Theme ID: <input type='text' name='theme_id' value='<?=$data->theme_id ?>' class='form-control' /></label>
+            <label>Theme ID <input type='text' name='theme_id' value='<?=$data->theme_id ?>' class='form-control' /></label>
              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-             <label>Gender: <input type='text' name='gender' value='<?=$data->gender ?>' class='form-control' /></label>
+             <label>Gender <input type='text' name='gender' value='<?=$data->gender ?>' class='form-control' /></label>
          </div>
          <div class='form-group'>
-             <label>Location: <input type='text' name='location' value='<?=$data->location ?>' class='form-control' /></label>
+             <label>Location <input type='text' name='location' value='<?=$data->location ?>' class='form-control' /></label>
              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-             <label>User Type: <input type='text' name='user_type' value='<?=$data->user_type ?>' class='form-control' /></label>
+             <label>User Type <input type='text' name='user_type' value='<?=$data->user_type ?>' class='form-control' /></label>
          </div>
+-->
            <div class="save-button">
                <input type='submit' name='action' value='&nbsp;&nbsp;Save Changes&nbsp;&nbsp;' class='btn btn-success' />
            </div>
